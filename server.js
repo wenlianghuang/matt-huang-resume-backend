b826a2a14b48b95a19423859d239a84b2c04ab9f
Matt Huang <wenliangmatt@gmail.com>
2025-03-30 21:17:55 +0800

    modified file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors({ origin: "https://matt-huang-resume.vercel.app/" })); // 允許 Next.js 前端訪問
+app.use(cors({ origin: "https://matt-huang-resume.vercel.app" })); // 允許 Next.js 前端訪問
 
 app.get("/api/data", (req, res) => {
   res.json({ message: "來自 Express 的回應！" });
