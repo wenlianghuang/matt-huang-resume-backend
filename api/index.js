e2d62c8b71e7d9f51f9d2648af2dbe7789a3f433
Matt Huang <wenliangmatt@gmail.com>
2025-03-30 20:26:51 +0800

    modified file

--- api/index.js
+++ api/index.js
@@ -9,6 +9,12 @@ app.get('/api', (req, res) => {
   res.json({ message: 'Welcome to the API with Matt!' });
 });
 
+app.get('/api/data',(req,res)=>{
+  res.json({message:'Hello from the backend!',data:[1,2,3,4,5]});
+});
+const cors = require('cors');
+app.use(cors());
+
 const port = process.env.PORT || 9070;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
