ede81f023e8829854a4992117ae732c2f59e1749
Matt Huang <wenliangmatt@gmail.com>
2025-04-01 18:34:57 +0800

    modified file

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.get("/api/data", (req, res) => {
   res.json({ message: "來自 Express 的回應！" });
 });
 */
+// 使用 JSON 解析中介軟體
 app.use("/api", apiRouter); // 使用 API 路由
 
 // Vercel 使用 PORT 3000 可能無效，改成 process.env.PORT
