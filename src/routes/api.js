ba0d485276f76b959dc2143359e232084786b651
Matt Huang <wenliangmatt@gmail.com>
2025-04-01 19:05:03 +0800

    modified file

--- src/routes/api.js
+++ src/routes/api.js
@@ -12,12 +12,18 @@ router.use(cors({
 */
 // middleware 1: Log the request time
 router.use((req, res, next) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  //console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  const logMessage = `[${new Date().toISOString()}] ${req.method} ${req.url}`;
+  console.log(logMessage);
+  res.locals.logMessage = logMessage; // 將日誌消息存儲在 res.locals 中，以便在後續中間件中使用
   next();
 });
 
 router.get('/data', (req, res) => {
-  res.json({ message: 'Acer 來自 Express 的回應！' });
+  res.json({ 
+    message: 'Acer 來自 Express 的回應！',
+    log: res.locals.logMessage // 返回日誌消息 
+  });
 });
 
 module.exports = router;
