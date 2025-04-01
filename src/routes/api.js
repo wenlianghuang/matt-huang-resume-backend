22384556ef111743a37171efbdbe3df61ec3e882
Matt Huang <wenliangmatt@gmail.com>
2025-04-01 19:15:17 +0800

    modified file

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,14 +2,14 @@ const express = require('express');
 const cors = require('cors'); // 引入 cors 中間件
 const router = express.Router();
 
-/*
+
 // 配置 CORS，允許特定的來源
 router.use(cors({
   origin: 'https://matt-huang-resume.vercel.app', // 替換為你的前端 URL
   methods: ['GET', 'POST'], // 允許的 HTTP 方法
   allowedHeaders: ['Content-Type', 'Authorization'], // 允許的標頭
 }));
-*/
+
 // middleware 1: Log the request time
 router.use((req, res, next) => {
   //console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
