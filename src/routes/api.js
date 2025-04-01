6ddfca893896dc09aa6134be7c252405331c387f
Matt Huang <wenliangmatt@gmail.com>
2025-04-01 12:22:40 +0800

    modified file

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 // middleware 1: Log the request time
+/*
 router.use(req,res,next => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
   next();
 });
+*/
 router.get('/data', (req, res) => {
   res.json({ message: 'Acer 來自 Express 的回應！' });
 });
