ce5bd28ca6f1fd75f6a1fa6f48b59b8a5b3739e4
Matt Huang <wenliangmatt@gmail.com>
2025-04-02 12:24:40 +0800

    modified file

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const cors = require("cors");
 const apiRouter = require("./src/routes/api"); // 引入 API 路由
 const app = express();
 //app.use(cors({ origin: "https://matt-huang-resume.vercel.app" })); // 允許 Next.js 前端訪問
-router.use(cors({
+app.use(cors({
   origin: 'https://matt-huang-resume.vercel.app', // 替換為你的前端 URL
   methods: ['GET', 'POST','PUT','DELETE'], // 允許的 HTTP 方法
   allowedHeaders: ['Content-Type', 'Authorization'], // 允許的標頭
 }));
+app.use(express.json()); // 解析 JSON 請求主體
 /*
 app.get("/api/data", (req, res) => {
   res.json({ message: "來自 Express 的回應！" });
